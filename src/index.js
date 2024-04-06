ff391a45660fef5d89399c6bcaa87e852294e8f7
Wiktoria Skibińska <wikas2000@icloud.com>
2024-04-06 15:17:10 +0200

    code cleaning

--- src/index.js
+++ src/index.js
@@ -1,36 +1,7 @@
 let currentNumber = 1;
+document.querySelector("#slide" + currentNumber).classList.add("show");
+document.querySelector("#pin" + currentNumber).classList.add("selected");
 
-//slide5
-function showSlide5() {
-  showSlide(5);
-}
-const pin5 = document.querySelector("#pin5");
-pin5.addEventListener("click", showSlide5);
-//slide4
-function showSlide4() {
-  showSlide(4);
-}
-const pin4 = document.querySelector("#pin4");
-pin4.addEventListener("click", showSlide4);
-//slide3
-function showSlide3() {
-  showSlide(3);
-}
-const pin3 = document.querySelector("#pin3");
-pin3.addEventListener("click", showSlide3);
-//slide2
-function showSlide2() {
-  showSlide(2);
-}
-const pin2 = document.querySelector("#pin2");
-pin2.addEventListener("click", showSlide2);
-//slide1
-function showSlide1() {
-  showSlide(1);
-}
-const pin1 = document.querySelector("#pin1");
-pin1.addEventListener("click", showSlide1);
-///
 //next
 document.querySelector("#next").addEventListener("click", handleNext);
 
@@ -42,6 +13,7 @@ function handleNext() {
   showSlide(newNumber);
 }
 //previous
+document.querySelector("#prev").addEventListener("click", handlePrevious);
 function handlePrevious() {
   let newNumber = currentNumber - 1;
   if (newNumber < 1) {
@@ -49,12 +21,25 @@ function handlePrevious() {
   }
   showSlide(newNumber);
 }
-document.querySelector("#prev").addEventListener("click", handlePrevious);
+
 //ultimate showSlide
 
-function showSlide(newNumber) {
+const showSlide = (newNumber) => {
   document.querySelector(".show").classList.remove("show");
   document.querySelector("#slide" + newNumber).classList.add("show");
+
   currentNumber = newNumber;
+  showPin(newNumber);
   console.log(currentNumber);
+};
+
+function showPin(newNumber) {
+  document.querySelector(".selected").classList.remove("selected");
+  document.querySelector("#pin" + newNumber).classList.add("selected");
 }
+for (let numerPinu = 1; numerPinu <= 5; numerPinu++) {
+  document.querySelector("#pin" + numerPinu).addEventListener("click", () => {
+    showSlide(numerPinu);
+  });
+}
+///
