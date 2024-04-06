428ccb11d66ef22e10823e25ed02edb0d055a1d3
Wiktoria Skibińska <wikas2000@icloud.com>
2024-04-06 13:33:15 +0200

    code cleaning

--- src/index.js
+++ src/index.js
@@ -2,41 +2,31 @@ let currentNumber = 1;
 
 //slide5
 function showSlide5() {
-  const slide5 = document.querySelector("#slide5");
-  document.querySelector(".show").classList.remove("show");
-  slide5.classList.add("show");
+  showSlide(5);
 }
 const pin5 = document.querySelector("#pin5");
 pin5.addEventListener("click", showSlide5);
 //slide4
 function showSlide4() {
-  const slide4 = document.querySelector("#slide4");
-  document.querySelector(".show").classList.remove("show");
-  slide4.classList.add("show");
+  showSlide(4);
 }
 const pin4 = document.querySelector("#pin4");
 pin4.addEventListener("click", showSlide4);
 //slide3
 function showSlide3() {
-  const slide3 = document.querySelector("#slide3");
-  document.querySelector(".show").classList.remove("show");
-  slide3.classList.add("show");
+  showSlide(3);
 }
 const pin3 = document.querySelector("#pin3");
 pin3.addEventListener("click", showSlide3);
 //slide2
 function showSlide2() {
-  const slide2 = document.querySelector("#slide2");
-  document.querySelector(".show").classList.remove("show");
-  slide2.classList.add("show");
+  showSlide(2);
 }
 const pin2 = document.querySelector("#pin2");
 pin2.addEventListener("click", showSlide2);
 //slide1
 function showSlide1() {
-  const slide1 = document.querySelector("#slide1");
-  document.querySelector(".show").classList.remove("show");
-  slide1.classList.add("show");
+  showSlide(1);
 }
 const pin1 = document.querySelector("#pin1");
 pin1.addEventListener("click", showSlide1);
@@ -45,22 +35,26 @@ pin1.addEventListener("click", showSlide1);
 document.querySelector("#next").addEventListener("click", handleNext);
 
 function handleNext() {
-  document.querySelector(".show").classList.remove("show");
   let newNumber = currentNumber + 1;
   if (newNumber > 5) {
     newNumber = 1;
   }
-  document.querySelector("#slide" + newNumber).classList.add("show");
-  currentNumber = newNumber;
+  showSlide(newNumber);
 }
 //previous
 function handlePrevious() {
-  document.querySelector(".show").classList.remove("show");
   let newNumber = currentNumber - 1;
   if (newNumber < 1) {
     newNumber = 5;
   }
-  document.querySelector("#slide" + newNumber).classList.add("show");
-  currentNumber = newNumber;
+  showSlide(newNumber);
 }
 document.querySelector("#prev").addEventListener("click", handlePrevious);
+//ultimate showSlide
+
+function showSlide(newNumber) {
+  document.querySelector(".show").classList.remove("show");
+  document.querySelector("#slide" + newNumber).classList.add("show");
+  currentNumber = newNumber;
+  console.log(currentNumber);
+}
